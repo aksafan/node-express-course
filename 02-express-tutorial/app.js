4616664ef1308b4407e8a3c8d2314abfd43bc553
aksafan <subzero_ck@ukr.net>
2025-01-22 22:58:27 -0600

    answers for lesson 4

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,16 +1,61 @@
 const express = require("express");
 const path = require("path");
-const {products} = require("./data");
+const {products, people} = require("./data");
 const {filterName, filterDescription, filterPrice} = require("./utils/filters");
+const peopleRouter = require("./routes/people")
+const cookieParser = require("cookie-parser")
 
 const app = express();
 
-app.use(express.static("./public"));
+const logger = (req, res, next) => {
+    console.log(`Http method is: ${req.method}, url is: ${req.url}`);
+    next();
+};
+
+const auth = (req, res, next) => {
+    const name = req.cookies.name;
+    if (!name) {
+        res.status(401).json({success: false, message: "Unauthorized"});
+    } else {
+        req.user = name;
+
+        next();
+    }
+};
+
+app.use(express.static("./methods-public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(logger);
+app.use('/api/v1/people', peopleRouter);
+
+app.post('/logon', (req, res) => {
+    const {name} = req.body;
+
+    if (!name) {
+        res.status(400).json({success: false, message: "Please provide a name"});
+    } else {
+        res.cookie("name", req.body.name)
+
+        res.status(201).json({success: true, message: `Hello ${name}`});
+    }
+});
+
+app.delete('/logoff', (req, res) => {
+    res.clearCookie("name")
+
+    res.status(200).json({success: true, message: `User was logged off`});
+});
+
+app.get('/test', auth, (req, res) => {
+    res.status(200).json({success: true, message: `Welcome ${req.user}`});
+});
 
 app.get('/api/v1/test', (req, res) => {
     res.status(200);
     res.json({message: "It worked!"});
-})
+});
 
 app.get('/api/v1/products/:productID', (req, res) => {
     const idToFind = parseInt(req.params.productID);
@@ -20,7 +65,7 @@ app.get('/api/v1/products/:productID', (req, res) => {
     } else {
         res.status(200).json({product});
     }
-})
+});
 
 // Example http://localhost:3000/api/v1/query?name=al&description=I%27m%20baby%20&price=[gt]=10.99&limit=5
 app.get('/api/v1/query', (req, res) => {
@@ -38,12 +83,12 @@ app.get('/api/v1/query', (req, res) => {
             "products": productsToReturn.slice(0, limit ?? productsToReturn.length)
         });
     }
-})
+});
 
 app.get('/api/v1/products', (req, res) => {
     res.status(200);
     res.json({message: products});
-})
+});
 
 app.get('/', (req, res) => {
     res.status(200);
---
+++ 02-express-tutorial/routes/people.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const {
+    getPeople,
+    getPerson,
+    addPerson,
+    updatePerson,
+    deletePerson
+} = require("../controllers/people.js");
+
+router.get("/", getPeople);
+
+router.get("/:id", getPerson);
+
+router.post("/", addPerson);
+
+router.put("/:id", updatePerson);
+
+router.delete("/:id", deletePerson);
+
+module.exports = router;
