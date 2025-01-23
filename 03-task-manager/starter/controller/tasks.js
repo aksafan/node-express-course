e8b71e2bdbdb140445bdac35281902b5050789f6
aksafan <subzero_ck@ukr.net>
2025-01-23 16:41:00 -0600

    answers for lesson 5

---
+++ 03-task-manager/starter/controller/tasks.js
@@ -0,0 +1,30 @@
+const Task = require('./../models/task');
+
+const getAllTasks = (req, res) => {
+    res.send('all tasks');
+};
+
+const getTask = (req, res) => {
+    res.send('one task');
+};
+
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body);
+    res.status(201).json({task});
+};
+
+const updateTask = (req, res) => {
+    res.send('update task');
+};
+
+const deleteTask = (req, res) => {
+    res.send('delete task');
+};
+
+module.exports = {
+    getAllTasks,
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
+};
