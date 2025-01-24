1ae9a757c76a8d044f6254d399f4737f2e9ac2b8
aksafan <subzero_ck@ukr.net>
2025-01-23 20:54:13 -0600

    answers for lesson 6

--- 03-task-manager/starter/controller/tasks.js
+++ 03-task-manager/starter/controller/tasks.js
@@ -1,25 +1,54 @@
 const Task = require('./../models/task');
+const asyncWrapper = require('./../middleware/async');
+const {createCustomError} =  require('./../errors/custom-error');
 
-const getAllTasks = (req, res) => {
-    res.send('all tasks');
-};
+const getAllTasks = asyncWrapper(async (req, res) => {
+    const tasks = await Task.find({});
+    res.status(201).json({tasks});
+});
 
-const getTask = (req, res) => {
-    res.send('one task');
-};
+const getTask = asyncWrapper(async (req, res, next) => {
+    const {id} = req.params;
+    const task = await Task.findOne({_id: id});
+    if (!task) {
+        return next(createCustomError(`No task with id: ${id}`, 404));
+    }
 
-const createTask = async (req, res) => {
+    res.status(200).json({task});
+});
+
+const createTask = asyncWrapper(async (req, res) => {
     const task = await Task.create(req.body);
     res.status(201).json({task});
-};
+});
 
-const updateTask = (req, res) => {
-    res.send('update task');
-};
+const updateTask = asyncWrapper(async (req, res) => {
+    const {id} = req.params;
+    const body = req.body;
+    const task = await Task.findOneAndUpdate(
+        {_id: id},
+        body,
+        {
+            new: true,
+            runValidators: true
+        }
+    );
+    if (!task) {
+        return next(createCustomError(`No task with id: ${id}`, 404));
+    }
 
-const deleteTask = (req, res) => {
-    res.send('delete task');
-};
+    res.status(200).json({task});
+});
+
+const deleteTask = asyncWrapper(async (req, res) => {
+    const {id} = req.params;
+    const task = await Task.findOneAndDelete({_id: id});
+    if (!task) {
+        return next(createCustomError(`No task with id: ${id}`, 404));
+    }
+
+    res.status(200).json({msg: 'Task was deleted'});
+});
 
 module.exports = {
     getAllTasks,
